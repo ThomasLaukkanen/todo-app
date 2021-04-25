8b4a7b1556dbcebf0d8c59e69a25c5f9971f4052
Thomas <tumppu91@hotmail.com>
2021-04-25 22:44:30 +0200

    added v-for for statistic

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="about">
     <h4>You have been grateful for</h4>
+    <ul>
+      <li
+        :key="grateful.id"
+        v-for="grateful in $route.store.state.finnishedTodos"
+      >
+        {{ grateful }}
+      </li>
+    </ul>
     <h4>Things that would be great if you did</h4>
     <h4>Body observations</h4>
     <h4>Lessons</h4>
